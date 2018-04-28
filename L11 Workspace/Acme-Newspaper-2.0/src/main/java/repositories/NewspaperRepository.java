ea24e742fe7f9f38f37ab2114979cbcbd81eb46f
HisperianHub <Mario.biz.gon@gmail.com>
2018-04-28 11:29:58 +0200

    Agent en proceso

--- L11 Workspace/Acme-Newspaper-2.0/src/main/java/repositories/NewspaperRepository.java
+++ L11 Workspace/Acme-Newspaper-2.0/src/main/java/repositories/NewspaperRepository.java
@@ -1,3 +1,4 @@
+
 package repositories;
 
 import java.util.Collection;
@@ -9,24 +10,29 @@ import org.springframework.stereotype.Repository;
 import domain.Newspaper;
 
 @Repository
-public interface NewspaperRepository extends JpaRepository<Newspaper, Integer>{
+public interface NewspaperRepository extends JpaRepository<Newspaper, Integer> {
 
 	@Query("select n from Newspaper n where n.publicationDate <= current_date")
 	Collection<Newspaper> findPastNewspapers();
-	
+
 	@Query("select n from Newspaper n join n.articles a where n.publicationDate <= current_date and a.isFinal = false")
 	Collection<Newspaper> findPastNewspapersWithNonFinalArticle();
-	
+
 	@Query("select n from Newspaper n where(n.title LIKE %?1% or n.description LIKE %?1%) and n.publicationDate <= current_date")
 	Collection<Newspaper> findPerKeyword(String keyword);
-	
+
 	@Query("select n from Newspaper n join n.articles a where(n.title LIKE %?1% or n.description LIKE %?1%) and n.publicationDate <= current_date and a.isFinal = false")
 	Collection<Newspaper> findPerKeywordNonFinalArticle(String keyword);
-	
+
 	@Query("select n from Newspaper n where n.publisher.id = ?1")
 	Collection<Newspaper> findByPublisherId(int publisherId);
-	
+
 	@Query("select n from Newspaper n where n.publicationDate > current_date")
 	Collection<Newspaper> findNonPublished();
-	
+
+	//			ACME-NEWSPAPER 2.0
+
+	@Query("select sum(case when n.advertisements.size>0 = 1 then 1.0 else 0.0 end)/count(n)from Newspaper n")
+	float ratioNewspapersWithVsWithoutAdvertisements();
+
 }
