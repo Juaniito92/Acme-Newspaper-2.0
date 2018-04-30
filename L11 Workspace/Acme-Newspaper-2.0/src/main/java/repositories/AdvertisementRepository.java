bee74417e7b3695f6de0671b356e9677709b9e29
MAR2 <Mario.biz.gon@gmail.com>
2018-04-30 17:08:47 +0200

    Cosas de Advertisement

--- L11 Workspace/Acme-Newspaper-2.0/src/main/java/repositories/AdvertisementRepository.java
+++ L11 Workspace/Acme-Newspaper-2.0/src/main/java/repositories/AdvertisementRepository.java
@@ -9,4 +9,8 @@ import domain.Advertisement;
 @Repository
 public interface AdvertisementRepository extends JpaRepository<Advertisement, Integer> {
 
+	//TODO no hace ná todavía, to be remade in Lucene
+	// @Query("select a from Advertisement a where(a.title LIKE %?1%")
+	// Collection<Advertisement> findAllTabooWords(String tabooWords);
+
 }
